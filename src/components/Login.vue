0d5d105be3e12a19a61c4123778f5c2c5c8caaaa
Dion Pettaway <dionp@fullsail.edu>
2019-05-10 11:37:49 -0400

    Styled the login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,29 @@
 <template>
-    <h1>
-        Log In
-    </h1>
+    <div id="login">
+        <section>
+        <div class="col1">
+            <h3><i class="fas fa-search"></i> Follow your interests.</h3>
+            <h3><i class="fas fa-user-friends"></i> Hear what people are talking about.</h3>
+            <h3><i class="far fa-comment"></i> Join the conversation.</h3>
+        </div>
+        <div class="col2">
+            <form @submit.prevent>
+                <h1>Chattn</h1>
+                <p>Welcome Back!</p>
+
+                <input type="text" placeholder="example@email.com" id="email1">
+                <input type="password" placeholder="Enter Your Password" id="password1">
+
+                <button class="button">Log In</button>
+
+                <div class="extras">
+                    <a>Forgot Password</a>
+                    <a>Create An Account</a>
+                </div>
+            </form>
+        </div>
+        </section>
+    </div>
 </template>
 
 <script>
