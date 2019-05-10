416cdb8401a75b6501e5ce35a0c361349111da7d
Dion Pettaway <dionp@fullsail.edu>
2019-05-10 12:03:04 -0400

    created store actions and state

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -48,7 +48,7 @@ export default {
                 .catch(err => console.log(err))
         }
     }
-
+    
 }
 </script>
 
--- src/store.js
+++ src/store.js
@@ -1,10 +1,32 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+const fb = require("./FirebaseConfig");
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+	state: {
+		currentUser: null,
+		userProfile: {}
+	},
+	mutations: {
+		setCurrentUser(state, value) {
+			state.currentUser = value;
+		},
+		setUserProfile(state, value) {
+			state.userProfile = value;
+		}
+	},
+	actions: {
+		fetchUserProfile({ commit, state }) {
+			fb.userCollection
+				.doc(state.currentUser.uid)
+				.get()
+				.then(res => {
+					commit("setUserProfile", res.data());
+				})
+				.catch(err => console.log(err));
+		}
+	}
 });
