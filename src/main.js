659aa93d835c66092aa9e5fba79dd3743a43334f
Dion Pettaway <dionp@fullsail.edu>
2019-05-10 10:39:46 -0400

    Added firebase cloud storage had to figure out the correct way to wrap this in Vue'\s main js file

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+const fb = require("./FirebaseConfig.js");
+
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+let app;
+
+fb.auth.onAuthStateChanged(user => {
+	if (!app) {
+		app = new Vue({
+			router,
+			store,
+			render: h => h(App)
+		}).$mount("#app");
+	}
+});
