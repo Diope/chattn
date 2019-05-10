d217a649aeb31ce7414224a4c90a8ce334434a0c
Dion Pettaway <dionp@fullsail.edu>
2019-05-10 11:54:04 -0400

    Oh god wrong branch for this

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -11,10 +11,10 @@
                 <h1>Chattn</h1>
                 <p>Welcome Back!</p>
 
-                <input type="text" placeholder="example@email.com" id="email1">
-                <input type="password" placeholder="Enter Your Password" id="password1">
+                <input v-model.trim="loginForm.email" type="text" placeholder="example@email.com" id="email1">
+                <input v-model.trim="loginForm.password" type="password" placeholder="Enter Your Password" id="password1">
 
-                <button class="button">Log In</button>
+                <button @click="login" class="button">Log In</button>
 
                 <div class="extras">
                     <a>Forgot Password</a>
@@ -27,7 +27,27 @@
 </template>
 
 <script>
+const fb = require('../FirebaseConfig.js')
 export default {
+    data() {
+        return {
+            loginForm: {
+                email: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        login() {
+            fb.auth.signInWithEmailAndPassword(this.loginForm.email, this.loginForm.password)
+                .then(user => {
+                    this.$store.commit('setCurrentUser', user)
+                    this.$store.dispatch('fetchUserProfile')
+                    this.$store.push('/dashboard')
+                })
+                .catch(err => console.log(err))
+        }
+    }
 
 }
 </script>
