394d9b78029e61f9a81df7838ddbd006922b921b
Dion Pettaway <dionp@fullsail.edu>
2019-05-10 11:09:26 -0400

    Stylings

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "./assets/scss/main.scss";
+
 const fb = require("./FirebaseConfig.js");
 
 Vue.config.productionTip = false;
